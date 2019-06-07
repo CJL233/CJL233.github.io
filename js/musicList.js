8a5e06a1e2396b2058fa01579c0af0858cc62d4e
CJL233 <46913173+CJL233@users.noreply.github.com>
2019-06-07 09:19:03 +0800

    Add files via upload

--- js/musicList.js
+++ js/musicList.js
@@ -1,6 +1,6 @@
 
 var preM= '{ "musics" : [' +
-'{ "name":"October - Recollection" , "src":"http://m10.music.126.net/20190607000613/5f5e0cd7f1b74508ebc202094bcabfe5/ymusic/56f9/2798/db8c/6e53bed06e322ea82fd9615983c4dcb3.mp3" }' 
+'{ "name":"October - Recollection" , "src":"http://fs.open.kugou.com/11ae72ab9fa41f834ec497525143fd15/5cf9b855/G049/M03/05/11/0YYBAFYjYhCAG-yqACxyUXKP6mA638.mp3" } ' 
 +', { "name":"Ed - What Do I Know?" , "src":"http://fs.open.kugou.com/3f1463d994072878206d06121d1dc76a/5cf8eb55/G092/M05/11/17/_IYBAFi38zSACahlADnzMVOI41Q570.mp3" } ' 
-+', { "name":"OneRepublic - Apologize" , "src":"http://m10.music.126.net/20190606191721/7ac2a36dfcc04970db0a098674892ff9/ymusic/8239/5639/569c/8cec1abfd77c27448d146370d5140e69.mp3" } ' 
-+', { "name":"霍尊 - 天行九歌" , "src":"http://m10.music.126.net/20190606191251/82c26523f4617dd89ee55e46b680fe71/ymusic/fcf7/17e3/1e2d/879346c12fdf513d23d085a229bdf132.mp3" } ]}';
++', { "name":"OneRepublic - Apologize" , "src":"http://fs.open.kugou.com/4a99eaeabfc221c74ed967f714b9ec24/5cf9b7d0/part/0/964146/G005/M02/04/11/RQ0DAFT-YeqAGPIvADLxzXkWTnw313.mp3" } ' 
++', { "name":"霍尊 - 天行九歌" , "src":"http://fs.open.kugou.com/1074ef0800341be918488320e028e633/5cf9b94f/G054/M02/11/17/FpQEAFcajLGAfhiVADk6dnvWkBU139.mp3" } ]}';
