2aacb8ad3fdeb92b71df6845735d1ed4068dc4aa
CJL233 <46913173+CJL233@users.noreply.github.com>
2019-06-08 13:28:23 +0800

    Add files via upload

--- js/musicList.js
+++ js/musicList.js
@@ -1,6 +1,6 @@
 
 var preM= '{ "musics" : [' +
-'{ "name":"October - Recollection" , "src":"http://fs.open.kugou.com/11ae72ab9fa41f834ec497525143fd15/5cf9b855/G049/M03/05/11/0YYBAFYjYhCAG-yqACxyUXKP6mA638.mp3" } ' 
-+', { "name":"Ed - What Do I Know?" , "src":"http://fs.open.kugou.com/3f1463d994072878206d06121d1dc76a/5cf8eb55/G092/M05/11/17/_IYBAFi38zSACahlADnzMVOI41Q570.mp3" } ' 
-+', { "name":"OneRepublic - Apologize" , "src":"http://fs.open.kugou.com/4a99eaeabfc221c74ed967f714b9ec24/5cf9b7d0/part/0/964146/G005/M02/04/11/RQ0DAFT-YeqAGPIvADLxzXkWTnw313.mp3" } ' 
-+', { "name":"霍尊 - 天行九歌" , "src":"http://fs.open.kugou.com/1074ef0800341be918488320e028e633/5cf9b94f/G054/M02/11/17/FpQEAFcajLGAfhiVADk6dnvWkBU139.mp3" } ]}';
+'{ "name":"October - Recollection" , "src":"audio/Recollection.mp3" } ' 
++', { "name":"Ed - What Do I Know?" , "src":"audio/What Do I Know.mp3" } ' 
++', { "name":"OneRepublic - Apologize" , "src":"audio/Apologize.mp3" } ' 
++', { "name":"霍尊 - 天行九歌" , "src":"audio/天行九歌.mp3" } ]}';
